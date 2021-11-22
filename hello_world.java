5d35103c5a71bc8eabed1fd35c0230a310cf8fda
Travis-test <89125852+Arthana-Haridas@users.noreply.github.com>
2021-11-22 17:49:08 +0530

    Update hello_world.java
    fact

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,11 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello World aa"); 
+      int num = 99999999;
+BigInteger factorial = BigInteger.ONE;
+for(int i=1; i<=num; ==i)
+{
+factorial = factorial.multiply(BigInteger.valueOf(i));
+}
+System.out.printf("Factorial of %d = %d", num,factorial);
     }
 }
