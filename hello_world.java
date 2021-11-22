ba5148ee372a4bef6cc5256e51b4f5f9e14a49c2
Travis-test <89125852+Arthana-Haridas@users.noreply.github.com>
2021-11-22 18:30:23 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,11 +1,12 @@
 class HelloWorld {
     public static void main(String[] args) {
-      int num = 99999999;
-BigInteger factorial = BigInteger.ONE;
-for(int i=1; i<=num; ==i)
-{
-factorial = factorial.multiply(BigInteger.valueOf(i));
-}
-System.out.printf("Factorial of %d = %d", num,factorial);
+      int i;fact=1;
+      int number = 9999999;
+   for(i=1,i<=number; i++)
+   {
+   fact=fact*i;   
+   }
+System.out.println("Factorial of"+number+"is: "+fact);
+      
     }
 }
