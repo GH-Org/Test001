b139608b2ccc8f439908ff38ff7b4e4523d5e8d7
Travis-test <89125852+Arthana-Haridas@users.noreply.github.com>
2021-11-22 18:39:07 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
       int i;fact=1;
-      int number = 9999999;
+      int number = 999999;
    for(i=1,i<=number; i++)
    {
    fact=fact*i;   
