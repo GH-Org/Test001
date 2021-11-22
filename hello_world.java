44f3e1f072e451f9af39f70b81aee69799087821
Travis-test <89125852+Arthana-Haridas@users.noreply.github.com>
2021-11-22 19:28:33 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,12 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-      int i;fact=1;
-      int number = 999999999;
-   for(i=1,i<=number; i++)
-   {
-   fact=fact*i;   
-   }
-System.out.println("Factorial of"+number+"is: "+fact);
+      
+System.out.println("Hello World");
       
     }
 }
