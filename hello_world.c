e60304bf1cec0f2742cfd4a0347bf8e93811ab24
Travis-test <89125852+Arthana-Haridas@users.noreply.github.com>
2022-03-28 13:33:24 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,7 @@
 include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("\n\n Hello, World!! \n\n");
+   printf("\n\n Hello, World!! \n\n\n");
    printf("\n\n\n Hello, World!! \n\n\n");
    return 0;
 }
